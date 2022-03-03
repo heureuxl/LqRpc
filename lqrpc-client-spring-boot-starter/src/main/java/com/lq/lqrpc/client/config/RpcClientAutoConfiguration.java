1a1819479943a6b3d9d71cf4f43f16aa92472233
liuqi <liu.qi@asiainfo-sec.com>
2022-03-03 14:07:29 +0800

    commit

---
+++ lqrpc-client-spring-boot-starter/src/main/java/com/lq/lqrpc/client/config/RpcClientAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.lq.lqrpc.client.config;
+
+
+/**
+ * @ClassName: RpcClientAutoConfiguration
+ * @Description: TODO
+ * @author: liuqi
+ * @date: 2022/3/1 上午11:03
+ * @Version: 0.1
+ */
+public class RpcClientAutoConfiguration {
+}
---
+++ lqrpc-server-spring-boot-starter/src/main/java/com/lq/lqrpc/server/config/RpcServerAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.lq.lqrpc.server.config;
+
+/**
+ * @ClassName: RpcServerAutoConfiguration
+ * @Description: TODO
+ * @author: liuqi
+ * @date: 2022/3/3 上午10:19
+ * @Version: 0.0.1
+ */
+public class RpcServerAutoConfiguration {
+}
